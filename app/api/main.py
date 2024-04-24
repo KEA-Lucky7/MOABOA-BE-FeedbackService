8896d023ee8a1fe8cf7ca5cb5312f0ea884a4d78
hyunseok <hyun104467@naver.com>
2024-04-24 18:06:12 +0900

    KAN-159 [CHORE]: 피드백 서비스 초기 세팅

---
+++ app/api/main.py
@@ -0,0 +1,4 @@
+from fastapi import APIRouter
+
+apiRouter = APIRouter()
+apiRouter.include_router()
---
+++ main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+
+from app.api.main import apiRouter
+from app.core.config import settings
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+# Set all CORS enabled origins
+if settings.BACKEND_CORS_ORIGINS:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[
+            str(origin).strip("/") for origin in settings.BACKEND_CORS_ORIGINS
+        ],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+app.include_router(apiRouter, prefix="/api")
