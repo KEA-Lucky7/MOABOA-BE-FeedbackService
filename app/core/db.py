2c302a89a8ce632dda44d00feafddb37d5b085ef
hyunseok <hyun104467@naver.com>
2024-06-01 17:01:05 +0900

    KAN-257 [FEAT]: db 연결

--- app/core/db.py
+++ app/core/db.py
@@ -1,10 +1,14 @@
+import sys
+
+from starlette.config import Config
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from main import app
+# env = sys.argv[1] if len(sys.argv) > 1 else 'dev'
+config = Config('.env')
 
-SQLALCHEMY_DATABASE_URL = app.config.DATABASE_URI
+SQLALCHEMY_DATABASE_URL = config('DATABASE_URI', cast=str)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
