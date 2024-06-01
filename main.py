cb7836b460091dab87c5a1ef58480c8518326894
hyunseok <hyun104467@naver.com>
2024-06-01 17:07:02 +0900

    KAN-257 [FIX]: cors 오류 수정

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 import sys
 
-import uvicorn
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
 from app.api.routes.feedback import feedbackRouter
-from app.config.config import LocalConfig, DevelopmentConfig
-from app.core.config import settings
 from app.core.db import Base, engine
 
 app = FastAPI()
 app.include_router(feedbackRouter, prefix="/api/feedback", tags=["feedback"])
 
 Base.metadata.create_all(bind=engine)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @feedbackRouter.get("/")
 async def root():
@@ -21,26 +26,3 @@ async def root():
 @feedbackRouter.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
-
-
-# Set all CORS enabled origins
-if settings.BACKEND_CORS_ORIGINS:
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=[
-            str(origin).strip("/") for origin in settings.BACKEND_CORS_ORIGINS
-        ],
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
-
-if __name__ == '__main__':
-    env = sys.argv[1] if len(sys.argv) > 1 else 'dev'
-
-    if env == 'local':
-        app.config = LocalConfig
-    elif env == 'dev':
-        app.config = DevelopmentConfig
-    else:
-        raise ValueError('Invalid environment name')
