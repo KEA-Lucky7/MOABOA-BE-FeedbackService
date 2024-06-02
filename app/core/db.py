6ea0adf20286209b0b79408132cbadfedb05d90b
hyunseok <hyun104467@naver.com>
2024-06-03 01:09:11 +0900

    [CHORE]: 환경 변수 설정 패키지 변경

--- app/core/db.py
+++ app/core/db.py
@@ -1,9 +1,7 @@
-from starlette.config import Config
+from app.core.config import config
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
-config = Config('.env')
 
 SQLALCHEMY_DATABASE_URL = config('DATABASE_URI', cast=str)
 
