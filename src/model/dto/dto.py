7a4f653c38415238efc95b4b41ea6b971531ba86
hyunseok <hyun104467@naver.com>
2024-06-09 14:31:31 +0900

    [FIX]: 피드백 생성 로직 변경에 따른 api 변경

--- src/model/dto/dto.py
+++ src/model/dto/dto.py
@@ -39,16 +39,11 @@ class PostFeedbackCreate(PostFeedbackBase):
     pass
 
 
-class PostFeedbackVO(BaseModel):
-    id: int
-    post_id: int
-    feedback: str
-
-    class Config:
-        from_attributes = True
-
-
 class PostFeedbackRead(BaseModel):
     id: int
     post_id: int
     feedback: str
+
+
+class PostFeedbackUpdate(PostFeedbackBase):
+    pass
